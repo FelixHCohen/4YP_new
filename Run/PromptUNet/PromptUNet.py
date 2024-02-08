91b43a778f5d42653d5f45f236308d361fbc5756
Felix Cohen <felixcohen@Felixs-MacBook-Pro.local>
2024-02-08 17:06:04 +0000

    new commit

--- Run/PromptUNet/PromptUNet.py
+++ Run/PromptUNet/PromptUNet.py
@@ -154,16 +154,22 @@ class SymmetricPromptUNet(nn.Module):
         """ Encoder """
         self.e1 = encoder_block(in_c, base_c,batch_norm)
         self.e2 = encoder_block(base_c, base_c * kernels[0],batch_norm)
-        self.e3 = encoder_block(base_c * kernels[0], base_c * kernels[1],batch_norm)
-        self.e4 = encoder_block(base_c * kernels[1], base_c * kernels[2],batch_norm)
-        #self.e5 = encoder_block(base_c*kernels[2],base_c*kernels[3])
+
+        self.e3 = conv_block(base_c * kernels[0], base_c * kernels[1],batch_norm)
+        self.e3_p = torch.nn.MaxPool2d((2,2))
+        self.e4 = conv_block(base_c * kernels[1], base_c * kernels[2],batch_norm)
+        self.e4_p = torch.nn.MaxPool2d((2, 2))
+
+
         """ Bottleneck """
-        #self.b1 = conv_block(base_c * kernels[3], base_c * kernels[4])
+
         self.b = conv_block(base_c * kernels[2], base_c * kernels[3],batch_norm)
 
+        self.promptImageCrossAttention_e3 = ImageEncoder(self.device, self.pe_layer, d_model=self.d_model,d_image_in=self.d_image_in // 4, num_heads=num_heads,num_blocks=attention_kernels[3], dropout=dropout,batch=batch_norm)
+        self.promptImageCrossAttention_e4 = ImageEncoder(self.device, self.pe_layer, self.d_model, self.d_image_in // 2,num_heads, attention_kernels[2], dropout, batch=batch_norm)
 
-        self.promptImageCrossAttention_post = ImageEncoder(self.device, self.pe_layer, d_model = self.d_model, d_image_in = self.d_image_in//2,num_heads = num_heads, num_blocks = attention_kernels[2],dropout = dropout,batch=batch_norm)
-        self.promptImageCrossAttention_post_post = ImageEncoder(self.device, self.pe_layer, self.d_model, self.d_image_in // 4,num_heads,attention_kernels[3], dropout,batch=batch_norm)
+        self.promptImageCrossAttention_d1 = ImageEncoder(self.device, self.pe_layer, d_model = self.d_model, d_image_in = self.d_image_in//2,num_heads = num_heads, num_blocks = attention_kernels[2],dropout = dropout,batch=batch_norm)
+        self.promptImageCrossAttention_d2 = ImageEncoder(self.device, self.pe_layer, self.d_model, self.d_image_in // 4,num_heads,attention_kernels[3], dropout,batch=batch_norm)
         #self.crossattention_final = ImageEncoder(self.device,self.pe_layer,self.d_model,self.d_image_in//8,num_heads,2,dropout,)
         """ Decoder """
        # self.dnew = decoder_block(base_c * kernels[4], base_c * kernels[3])
@@ -186,9 +192,13 @@ class SymmetricPromptUNet(nn.Module):
         """ Encoder """
         s1, p1 = self.e1(images)
         s2, p2 = self.e2(p1)
-        s3, p3 = self.e3(p2)
-        s4, p4 = self.e4(p3)
 
+        s3,prompts_d2 = self.promptImageCrossAttention_e3(self.e3(p2),prompts,original_prompts)
+        p3 = self.e3_p(s3)
+
+
+        s4, prompts_d1 = self.promptImageCrossAttention_e4(self.e4(p3),prompts,original_prompts)
+        p4 = self.e4_p(s4)
         """ Bottleneck """
 
         b = self.b(p4)
@@ -199,13 +209,13 @@ class SymmetricPromptUNet(nn.Module):
         d1 = self.d1(b, s4)
 
         if train_attention:
-            d1, prompts = self.promptImageCrossAttention_post(d1, prompts,original_prompts)
+            d1, prompts = self.promptImageCrossAttention_d1(d1, prompts_d1,original_prompts)
 
 
         d2 = self.d2(d1, s3)
 
         if train_attention:
-            d2,prompts = self.promptImageCrossAttention_post_post(d2,prompts,original_prompts,)
+            d2,prompts = self.promptImageCrossAttention_d2(d2,prompts_d2,original_prompts,)
 
 
         d3 = self.d3(d2, s2)
